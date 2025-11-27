d10f0c3a01da394df94c4739b7fe612c9a4b5b94
seu nome de usuárioDantdddd <dantedpagliarini@gmail.com>
2025-11-26 21:50:47 -0300

    Fixed port issue

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import os
 
 app = Flask(__name__)
 
@@ -48,4 +49,5 @@ def buscar():
     return render_template("buscar.html", resultados=resultados)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
